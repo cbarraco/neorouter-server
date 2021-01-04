87b85c077c1dac12bd0eae7ba5cca0cc7c7c303b
Carlo Barraco <carlobarraco@gmail.com>
2021-01-03 21:03:00 -0500

    Changed path

--- init.sh
+++ init.sh
@@ -1,6 +1,1 @@
-# By default there is nothing in dbroot so nr fails to start.
-# This will populate dbroot on first startup.
-if [ ! -f "/data/NeoRouter_0_0_1.db" ]; then
-    mv /usr/local/ZebraNetworkSystems/NeoRouter/* /data/
-fi
-/usr/bin/nrserver -run --dbroot /data
+/usr/bin/nrserver -run
