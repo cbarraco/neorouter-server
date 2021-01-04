44145d21ab292eae94d729aff47e19cb671960ff
Carlo Barraco <carlobarraco@gmail.com>
2021-01-03 21:26:23 -0500

    Reverting path

--- init.sh
+++ init.sh
@@ -1,1 +1,6 @@
-/usr/bin/nrserver -run
+# By default there is nothing in dbroot so nr fails to start.
+# This will populate dbroot on first startup.
+if [ ! -f "/data/NeoRouter_0_0_1.db" ]; then
+    mv /usr/local/ZebraNetworkSystems/NeoRouter/* /data/
+fi
+/usr/bin/nrserver -run --dbroot /data
